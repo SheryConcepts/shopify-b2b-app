698b84451bdd621efc7bac67399776c943087e65
Shery <chsheharyarahmed@gmail.com>
2024-03-23 23:39:25 +0500

    some padding adjustment

--- app/b2b-customers-manage/create/page.tsx
+++ app/b2b-customers-manage/create/page.tsx
@@ -7,6 +7,7 @@ import {
   Page,
   Card,
   Button,
+  Box,
 } from "@shopify/polaris";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
@@ -20,7 +21,7 @@ export default function CreatePage() {
     sdf: z.string().trim().min(1, "First Name is required").nonempty(),
     lastName: z.string().trim().min(1, "Last Name is required").nonempty(),
     email: z.string().email("Invalid email").nonempty(),
-    password: z.string().trim().min(1,"Password is required").nonempty(),
+    password: z.string().trim().min(1, "Password is required").nonempty(),
     phone: z.string().optional(),
     address: z.string().trim().optional(),
     note: z.string().optional(),
@@ -107,74 +108,76 @@ export default function CreatePage() {
       }}
       title="Create Customer"
     >
-      <Card>
-        <Form onSubmit={handleSubmit}>
-          <FormLayout>
-            <TextField
-              id="firstName"
-              autoComplete=""
-              label="First Name"
-              value={formData.firstName}
-              onChange={handleChange}
-              error={validationErrors.firstName}
-            />
-            <TextField
-              id="lastName"
-              autoComplete=""
-              label="Last Name"
-              value={formData.lastName}
-              onChange={handleChange}
-              error={validationErrors.lastName}
-            />
-            <TextField
-              id="email"
-              autoComplete=""
-              label="Email"
-              type="email"
-              value={formData.email}
-              onChange={handleChange}
-              error={validationErrors.email}
-            />
-            <TextField
-              id="password"
-              autoComplete=""
-              label="Password"
-              type="text"
-              value={formData.password}
-              onChange={handleChange}
-              error={validationErrors.password}
-            />
-            <TextField
-              id="phone"
-              autoComplete=""
-              label="Phone"
-              type="tel"
-              error={validationErrors.phone}
-              value={formData.phone}
-              onChange={handleChange}
-            />
-            <TextField
-              id="address"
-              autoComplete=""
-              label="Address"
-              multiline
-              value={formData.address}
-              error={validationErrors.address}
-              onChange={handleChange}
-            />
-            <TextField
-              id="note"
-              autoComplete=""
-              label="Note"
-              multiline
-              error={validationErrors.note}
-              value={formData.note}
-              onChange={handleChange}
-            />
-            <Button submit>Create Customer</Button>
-          </FormLayout>
-        </Form>
-      </Card>
+      <Box paddingBlockEnd={"400"}>
+        <Card>
+          <Form onSubmit={handleSubmit}>
+            <FormLayout>
+              <TextField
+                id="firstName"
+                autoComplete=""
+                label="First Name"
+                value={formData.firstName}
+                onChange={handleChange}
+                error={validationErrors.firstName}
+              />
+              <TextField
+                id="lastName"
+                autoComplete=""
+                label="Last Name"
+                value={formData.lastName}
+                onChange={handleChange}
+                error={validationErrors.lastName}
+              />
+              <TextField
+                id="email"
+                autoComplete=""
+                label="Email"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
+                error={validationErrors.email}
+              />
+              <TextField
+                id="password"
+                autoComplete=""
+                label="Password"
+                type="text"
+                value={formData.password}
+                onChange={handleChange}
+                error={validationErrors.password}
+              />
+              <TextField
+                id="phone"
+                autoComplete=""
+                label="Phone"
+                type="tel"
+                error={validationErrors.phone}
+                value={formData.phone}
+                onChange={handleChange}
+              />
+              <TextField
+                id="address"
+                autoComplete=""
+                label="Address"
+                multiline
+                value={formData.address}
+                error={validationErrors.address}
+                onChange={handleChange}
+              />
+              <TextField
+                id="note"
+                autoComplete=""
+                label="Note"
+                multiline
+                error={validationErrors.note}
+                value={formData.note}
+                onChange={handleChange}
+              />
+              <Button submit>Create Customer</Button>
+            </FormLayout>
+          </Form>
+        </Card>
+      </Box>
     </Page>
   );
 }
